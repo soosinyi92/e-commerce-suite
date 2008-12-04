c0390ee2353c3e9ac436d8a9a56177216f7b3063
sitn.binome.99 <sitn.binome.99@092c312c-c20d-11dd-8bad-cde060d8fd3c>
2008-12-04 16:55:11 +0000

    model

---
+++ e-commerce-suite-project/src/java/fr/dauphine/ecommerce/model/CartItem.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.dauphine.ecommerce.model;
+
+/**
+ *
+ * @author DELL
+ */
+public class CartItem {
+
+}
