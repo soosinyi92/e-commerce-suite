29a645a54750395dc8468186457ea8a5a6367201
sitn.binome.99 <sitn.binome.99@092c312c-c20d-11dd-8bad-cde060d8fd3c>
2008-12-12 16:03:55 +0000

    model

--- e-commerce-suite-project/src/java/fr/dauphine/ecommerce/model/Cart.java
+++ e-commerce-suite-project/src/java/fr/dauphine/ecommerce/model/Cart.java
@@ -5,10 +5,27 @@
 
 package fr.dauphine.ecommerce.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author DELL
  */
 public class Cart {
+
+    private List<CartItem> items;
+
+    public Cart() {
+        items = new ArrayList<CartItem>();
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
 
 }
--- e-commerce-suite-project/src/java/fr/dauphine/ecommerce/model/OrderItem.java
+++ e-commerce-suite-project/src/java/fr/dauphine/ecommerce/model/OrderItem.java
@@ -11,4 +11,26 @@ package fr.dauphine.ecommerce.model;
  */
 public class OrderItem {
 
+    private Product product;
+    private Integer quantity;
+
+    public OrderItem() {
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    
 }
